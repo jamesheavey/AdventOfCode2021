1aeb88dbbcb2a15ea56a347bde7bf80b944f1081
James Heavey <james.heavey@hq.trayport.com>
2021-12-17 17:54:37 +0000

    Puzzle 2 produces the correct output for parts 1 and 2

--- Puzzle2/Program.cs
+++ Puzzle2/Program.cs
@@ -1,2 +1,98 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿namespace Puzzle2
+{
+    public class Program
+    {
+        const string FilePath = @"C:\Users\james.heavey\source\repos\AdventOfCode2021\Puzzle2\Puzzle2Input.txt";
+        private static void Main()
+        {
+            Part1();
+
+            Part2();
+        }
+
+        private static void Part1()
+        {
+            var lines = File.ReadAllLines(FilePath);
+
+            var instructions = new List<string>();
+            var magnitudes = new List<int>();
+
+            foreach (var line in lines)
+            {
+                var split = line.Split(' ');
+                instructions.Add(split[0]);
+                magnitudes.Add(Convert.ToInt32(split[1]));
+            }
+
+            var submarine = new Submarine(0,0,0);
+
+            for (int i = 0; i < lines.Count(); i++)
+            {
+                switch (instructions[i])
+                {
+                    case "forward":
+                        submarine.X = submarine.X + magnitudes[i];
+                        break;
+
+                    case "up":
+                        submarine.Y = submarine.Y - magnitudes[i];
+                        break;
+
+                    case "down":
+                        submarine.Y = submarine.Y + magnitudes[i];
+                        break;
+
+                    default:
+                        break;
+                }
+            }
+
+            Console.WriteLine($"PART 1 - \nThe position is: {submarine.X}\n" +
+                $"The depth is: {submarine.Y}\n" +
+                $"The Answer is: {submarine.X*submarine.Y}");
+        }
+
+        public static void Part2()
+        {
+            var lines = File.ReadAllLines(FilePath);
+
+            var instructions = new List<string>();
+            var magnitudes = new List<int>();
+
+            foreach (var line in lines)
+            {
+                var split = line.Split(' ');
+                instructions.Add(split[0]);
+                magnitudes.Add(Convert.ToInt32(split[1]));
+            }
+
+            var submarine = new Submarine(0, 0, 0);
+
+            for (int i = 0; i < lines.Count(); i++)
+            {
+                switch (instructions[i])
+                {
+                    case "forward":
+                        submarine.X = submarine.X + magnitudes[i];
+                        submarine.Y = submarine.Y + (magnitudes[i] * submarine.AIM);
+                        break;
+
+                    case "up":
+                        submarine.AIM = submarine.AIM - magnitudes[i];
+                        break;
+
+                    case "down":
+                        submarine.AIM = submarine.AIM + magnitudes[i];
+                        break;
+
+                    default:
+                        break;
+                }
+            }
+
+            Console.WriteLine($"PART 2 - \nThe position is: {submarine.X}\n" +
+                $"The depth is: {submarine.Y}\n" +
+                $"The Answer is: {submarine.X * submarine.Y}");
+        }
+    }
+}
---
+++ Puzzle2/Submarine.cs
@@ -0,0 +1,13 @@
+﻿public struct Submarine
+{
+    public Submarine(double x, double y, double aim)
+    {
+        X = x;
+        Y = y;
+        AIM = aim;
+    }
+
+    public double X { get; set; }
+    public double Y { get; set; }
+    public double AIM { get; set; }
+}
