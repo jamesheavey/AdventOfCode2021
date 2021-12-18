fca2c170a3b36850cd331c4e3273cb638d52dc02
James Heavey <james@heavey.net>
2021-12-18 02:23:59 +0000

    Initialised Puzzle 3 project

---
+++ Puzzle3/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace Puzzle2
+{
+    public class Program
+    {
+        const string FileName = "Puzzle3Input.txt";
+        static string FilePath = Path.Combine(Directory.GetCurrentDirectory(), "..\\..\\..\\");
+
+        private static void Main()
+        {
+            var binaries = File.ReadAllLines(@$"{FilePath}\{FileName}")
+                .ToList();
+
+        }
+
+        private static void Part1()
+        {
+
+        }
+
+        public static void Part2()
+        {
+
+        }
+    }
+}
