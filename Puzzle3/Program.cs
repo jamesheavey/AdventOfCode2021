497024a244f4e8e82ac79333105cac59b92d12ca
James Heavey <james@heavey.net>
2021-12-19 17:08:58 +0000

    Part 2 works as expected

--- Puzzle3/Program.cs
+++ Puzzle3/Program.cs
@@ -25,11 +25,11 @@
             {
                 for (var i = 0; i < bin.Length; i++)
                 {
-                    counts[i] += bin[i] == '1' ? 1:0; 
+                    counts[i] += bin[i] == '1' ? 1 : 0;
                 }
             }
 
-            var readings = new string[2]; 
+            var readings = new string[2];
 
             foreach (var digit in counts)
             {
@@ -46,7 +46,33 @@
 
         public static void Part2()
         {
+            var CO2 = new List<string>(binaries);
+            var O2 = new List<string>(binaries);
 
+            for (var i = 0; i < CO2[0].Length; i++)
+            {
+                Console.WriteLine($"CO2: {CO2.Count}, O2: {O2.Count}");
+
+                if (CO2.Count == 1) { }
+                else
+                {
+                    var commonVal = CO2.Count(c => c[i] == '1') >= CO2.Count(c => c[i] == '0') ? '1' : '0';
+
+                    CO2.RemoveAll(x => x[i] == commonVal);
+                }
+
+                if (O2.Count == 1) { }
+                else
+                {
+                    var commonVal = O2.Count(c => c[i] == '1') >= O2.Count(c => c[i] == '0') ? '0' : '1';
+
+                    O2.RemoveAll(x => x[i] == commonVal);
+                }
+            }
+
+            Console.WriteLine($"PART 2 - \nThe O2 is: {CO2[0]}\n" +
+                $"The CO2 is: {O2[0]}\n" +
+                $"The Answer is: {Convert.ToInt32(CO2[0], 2) * Convert.ToInt32(O2[0], 2)}");
         }
     }
 }
